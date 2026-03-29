1b374c390fa84c2a817eac8ee89aaa8c4fec45f9
unknown <sourabhc608@gmail.com>
2026-03-29 15:56:59 +0530

    29-3-2026,123

--- backend/app.js
+++ backend/app.js
@@ -8,27 +8,12 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://dulcet-malasada-7ed9bf.netlify.app"
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
+  origin: 'https://dulcet-malasada-7ed9bf.netlify.app',
+  methods:'GET,POST'
+}
 
 app.use(cors(corsOptions));
-
 
 
 app.use(express.json());
