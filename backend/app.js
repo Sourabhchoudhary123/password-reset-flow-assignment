1a53554815c1282c263ebf98208e11c0332ae037
unknown <sourabhc608@gmail.com>
2026-03-29 15:32:19 +0530

    29-3-2026,1

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://dulcet-malasada-7ed9bf.netlify.app"
+    "dulcet-malasada-7ed9bf.netlify.app"
   ],
   credentials: true
 }));
