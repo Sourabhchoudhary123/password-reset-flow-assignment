d2dc126ab31551eb8dcd80d0229c4a13992e3bbd
unknown <sourabhc608@gmail.com>
2026-03-30 17:14:04 +0530

    45455888888

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 export const getUsers = async (req, res) => {
   try {
     const users = await User.find();
-    res.json(users);
+    res.json(users);  
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
