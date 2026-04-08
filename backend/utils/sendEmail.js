97bed4cfb5dfed9c1976276bba97191ef4505529
unknown <sourabhc608@gmail.com>
2026-04-08 14:51:11 +0530

    meu4555

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,32 +1,29 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async (email, link) => {
-    try {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
 
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Password Reset",
+      text: `Click here to reset password: ${link}`
+    });
 
-        const transporter = nodemailer.createTransport({
-
-            service: "gmail",
-
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            }
-
-        });
-
-        await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: "Password Reset",
-            text: `Click here to reset password: ${link}`
-
-        });
-        console.log("Email sent successfully")
-    } catch (error) {
-        console.log("Send email error", error);
-        throw error
-    }
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("Send email error", error);
+    throw error;
+  }
 };
 
 export default sendEmail;
